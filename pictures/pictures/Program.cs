04b8ca11ef4bee5273864ffe65223836d5f2de17
AvanMyers <smail1341@gmail.com>
2022-08-05 19:42:35 +0300

    pictures

---
+++ pictures/pictures/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int allPictures = 52;
+            int linedUp = 3;
+
+            int beyondMeasure = allPictures % linedUp;
+            int result = allPictures / linedUp;
+
+            Console.WriteLine($"Полностью заполненных рядов картнок {result}" +
+                $", а картинок свер меры {beyondMeasure}");
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
